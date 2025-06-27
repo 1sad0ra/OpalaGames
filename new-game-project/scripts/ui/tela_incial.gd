381ee4ef5c5d744d2ff9d641bbf6f9d1d44f0897
1sad0ra <isadora.cd13@gmail.com>
2025-06-27 00:30:07 -0300

    Adicionando nova funcionalidade

---
+++ new-game-project/scripts/ui/tela_incial.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+
+func _on_jogar_button_1_pressed():
+	get_tree().change_scene_to_file("res://cenas/teste/world_player.tscn")
+
+func _on_conf_button_2_pressed():
+	pass # Replace with function body.
+
+func _on_sair_button_3_pressed():
+	get_tree().quit()
