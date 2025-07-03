440ab535f75248f33724b323d306b355a46a7212
santiago-pl01 <palomasantiago134@gmail.com>
2025-07-03 11:16:16 -0300

    add_novos ajustes

--- new-game-project/cenas/caracteristicas/player/walk_state.gd
+++ new-game-project/cenas/caracteristicas/player/walk_state.gd
@@ -4,7 +4,7 @@ extends NodeState
 #cria uma variavel para o sprit
 @export var animated_sprit2d :AnimatedSprite2D
 #variavel para a velocoade 
-@export var speed : int = 150
+@export var speed : int = 140
 @export var passos: AudioStreamPlayer2D 
 
 
